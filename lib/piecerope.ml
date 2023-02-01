e24bdbe7540f1ce8dcaba0e44009deb76095006b
Humza Shahid <humza@pop-os.localdomain>
2023-02-01 20:28:44 +0000

    start piecerope.ml

---
+++ lib/piecerope.ml
@@ -0,0 +1,92 @@
+open Buffer
+
+type node = {
+  start: int;
+  length: int;
+  left_idx: int;
+  right_idx: int;
+  left_lns: int;
+  right_lns: int;
+  lines: int array;
+}
+
+type tree = 
+  | PE
+  | PT of int * tree * node * tree
+
+type t = {
+  buffer: Buffer.t;
+  pieces: tree;
+}
+
+let create start length lines = { 
+  start = start; 
+  length = length; 
+  left_idx = 0; 
+  right_idx = 0;
+  left_lns = 0;
+  right_lns = 0; 
+  lines = lines; 
+}
+
+let addLeft idxDelta lnDelta node =
+  { node with left_idx = node.left_idx + idxDelta; left_lns = node.left_lns + lnDelta }
+
+let addRight idxDelta lnDelta node =
+  { node with right_idx = node.right_idx + idxDelta; right_lns = node.right_lns + lnDelta }
+
+let setIdx left right node =
+  { node with left_idx = left; right_idx = right; }
+
+let setData leftSize leftLines rightSize rightLines node =
+  { node with 
+      left_idx = leftSize;
+      left_lns = leftLines; 
+      right_idx = rightSize;
+      right_lns = rightLines; }
+  
+let nLength node = 
+  match node with
+  | PE -> 0
+  | PT(_, _, v, _) -> v.length
+
+let nLines node =
+  match node with
+  | PE -> 0
+  | PT(_, _, v, _) -> Array.length v.lines
+
+let size node =
+  match node with
+  | PE -> 0
+  | PT(_, _, v, _) -> v.left_idx + v.right_idx + v.length
+
+let idxLnSize node =
+  match node with
+  | PE -> 0, 0
+  | PT(_, _, v, _) -> v.left_idx + v.right_idx + v.length,
+                      v.left_lns + v.right_lns + Array.length v.lines
+
+let stringLength node =
+  match node with
+  | PE -> 0
+  | PT(_, _, v, _) -> v.length
+
+let sizeLeft node = 
+  match node with 
+  | PE -> 0
+  | PT(_, _, v, _) -> v.left_idx
+
+let sizeRight node = 
+  match node with 
+  | PE -> 0
+  | PT(_, _, v, _) -> v.right_idx
+
+let linesLeft node =
+  match node with
+  | PE -> 0
+  | PT(_, _, v, _) -> v.left_lns
+
+let linesRight node =
+  match node with
+  | PE -> 0
+  | PT(_, _, v, _) -> v.right_lns
