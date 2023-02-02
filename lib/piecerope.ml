d8b48d3dafb9b623f416b6b3c4230b4ecc9b04bc
Humza Shahid <humza@pop-os.localdomain>
2023-02-02 13:58:51 +0000

    complete piece logic code

--- lib/piecerope.ml
+++ lib/piecerope.ml
@@ -1,5 +1,3 @@
-open Buffer
-
 type node = {
   start: int;
   length: int;
@@ -91,19 +89,20 @@ let linesRight node =
   | PE -> 0
   | PT(_, _, v, _) -> v.right_lns
 
-(* Logic for handling piece nodes. *)
-let splitLines rStart (lines: int array) =
-  let rec findIndex pos =
-    if pos >= Array.length lines then
-      None
-    else
-      let cur = Array.get lines pos in
-      if cur >= rStart then
-        Some pos
-      else
-        findIndex (pos + 1)
+(* Logic for handling piece nodes; not using a separate module because more work due to defining interface. *)
+
+(* tryFindIndex function ported from F#: https://github.com/dotnet/fsharp/blob/main/src/FSharp.Core/array.fs#L1558 *)
+let tryFindIndex (predicate: int -> bool) (lines: int array) =
+  let len = Array.length lines in
+  let rec find n =
+    if n >= len then None
+    else if predicate (Array.unsafe_get lines n) then Some n
+    else find (n + 1)
   in
-  match findIndex 0 with
+  find 0
+
+let splitLines rStart (lines: int array) =
+  match tryFindIndex (fun x -> x >= rStart) lines with
   | Some splitPoint ->
       let arrLeft = Array.sub lines 0 (splitPoint - 1) in
       let arrRight = Array.sub lines splitPoint (Array.length lines - splitPoint - 1) in
@@ -111,3 +110,62 @@ let splitLines rStart (lines: int array) =
   | None ->
       lines, Array.make 0 0
 
+let deleteLinesInRange p1Length p2Start lines =
+  let p1Lines =
+    match tryFindIndex (fun x -> x >= p1Length) lines with
+    | Some x -> Array.sub lines 0 (x - 1)
+    | None -> Array.make 0 0
+  in
+  let p2Lines =
+    match tryFindIndex (fun x -> x >= p2Start) lines with
+    | Some x -> Array.sub lines x (Array.length lines - 1 - x)
+    | None -> Array.make 0 0
+  in
+  p1Lines, p2Lines
+
+let deleteInRange curIndex start finish piece =
+  let finishDifference = finish - curIndex in
+  let p1Length = start - curIndex in
+  let p2Start = finishDifference + piece.start in
+  let (p1Lines, p2Lines) = deleteLinesInRange (p1Length + piece.start) p2Start piece.lines in
+  let p2Length = piece.length - finishDifference in
+  (p1Length, p1Lines, p2Start, p2Length, p2Lines)
+
+let deleteAtStart curIndex finish piece =
+  let difference = finish - curIndex in
+  let newStart = piece.start + difference in
+  let newLength = piece.length - difference in
+  let newLines =
+    match tryFindIndex (fun x -> x >= difference) piece.lines with
+    | Some x -> Array.sub piece.lines x (Array.length piece.lines - 1 - x)
+    | None -> Array.make 0 0 
+  in
+  (newStart, newLength, newLines)
+
+let deleteAtEnd curIndex start piece =
+  let length = start - curIndex in
+  let lines = 
+    match tryFindIndex (fun x -> x <= length) piece.lines with
+    | Some x -> Array.sub piece.lines x (Array.length piece.lines - 1 - x)
+    | None -> Array.make 0 0
+  in
+  (length, lines)
+
+let text piece rope = Buffer.substring piece.start piece.length rope.buffer
+
+let textInRange curIndex start finish piece table =
+  let textStart = start - curIndex + piece.start in
+  let textLength = finish - curIndex + piece.start - textStart in
+  Buffer.substring textStart textLength table.buffer
+
+let textAtStart curIndex finish piece table =
+  let textLength = finish - curIndex in
+  Buffer.substring piece.start textLength table.buffer
+
+let textAtEnd curIndex start piece table =
+  let textStart = start - curIndex + piece.start in
+  let textLength = piece.start + piece.length - textStart in
+  Buffer.substring textStart textLength table.buffer
+
+let atStartAndLength start length table =
+  Buffer.substring start length table.buffer
