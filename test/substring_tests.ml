20550800356991a5e945bd309f01abb4d1f57628
Humza Shahid <humza@pop-os.localdomain>
2023-02-14 19:05:01 +0000

    done porting over all substring tests (3 failing; find out why)

--- test/substring_tests.ml
+++ test/substring_tests.ml
@@ -66,6 +66,65 @@ let test_can_get_substring_from_end_when_zipper_is_at_end _ =
   let expected = insText in
   assert_equal ~printer:print_text expected ropeSub
 
+let test_can_get_a_substring_from_start_when_zipper_is_at_middle _ =
+  let ropeSub = Piece_rope.insert (String.length text / 2) insText initialTable
+  |> Piece_rope.substring 0 5 in
+  let expected = String.sub text 0 5 in
+  assert_equal ~printer:print_text expected ropeSub
+
+let test_can_get_substring_of_middle_when_zipper_is_at_middle _ =
+  let ropeSub = Piece_rope.insert (String.length text / 2) insText initialTable
+  |> Piece_rope.substring (String.length text / 2) (String.length insText) in
+  let expected = insText in
+  assert_equal ~printer:print_text expected ropeSub
+
+let test_can_get_a_substring_over_a_split_piece _ =
+  let ropeSub = Piece_rope.create "123456789"
+  |> Piece_rope.insert 0 "a"
+  |> Piece_rope.insert 2 "b"
+  |> Piece_rope.substring 0 3 in
+  let expected = "a1b" in
+  assert_equal ~printer:print_text expected ropeSub
+
+let text_edit_1 _ =
+  let rope = Piece_rope.create "124567893"
+  |> Piece_rope.insert 8 "98"
+  |> Piece_rope.insert 9 "ab" in
+  
+  (* Check that we constructed string as needed for test. *)
+  let ropeText = Piece_rope.get_text rope in
+  let expectedText = "124567899ab83" in
+  let _ = assert_equal ~printer:print_text expectedText ropeText in
+
+  let expectedStrSub = "124567899" in
+  let ropeSub = Piece_rope.substring 0 9 rope in
+  assert_equal ~printer:print_text expectedStrSub ropeSub
+
+let text_edit_2 _ =
+  let rope = Piece_rope.create "123456789"
+  |> Piece_rope.insert 8 "a"
+  |> Piece_rope.insert 9 "b"
+  |> Piece_rope.insert 7 "c"
+  |> Piece_rope.insert 8 "d"
+  |> Piece_rope.insert 6 "e"
+  |> Piece_rope.insert 7 "f" in
+  
+  let expectedText = "123456ef7cd8ab9" in
+  let ropeText = Piece_rope.get_text rope in
+  let _ = assert_equal ~printer:print_text expectedText ropeText in
+
+  let expectedSub = "123456e" in
+  let ropeSub = Piece_rope.substring 0 7 rope in
+  let _ = assert_equal ~printer:print_text expectedSub ropeSub in
+
+  let expectedSub = "123456ef" in
+  let ropeSub = Piece_rope.substring 0 8 rope in
+  let _ = assert_equal ~printer:print_text expectedSub ropeSub in
+
+  let expectedSub = "123456ef7" in
+  let ropeSub = Piece_rope.substring 0 9 rope in
+  assert_equal ~printer:print_text expectedSub ropeSub 
+
 (* List of test suites to export. *)
 let test_suite = 
   "Substring_tests" >::: [
@@ -79,5 +138,9 @@ let test_suite =
     "test_can_get_substring_from_start_when_zipper_is_at_end " >:: test_can_get_substring_from_start_when_zipper_is_at_end ;
     "test_can_get_substring_from_middle_when_zipper_is_at_end ">:: test_can_get_substring_from_middle_when_zipper_is_at_end ;
     "test_can_get_substring_from_end_when_zipper_is_at_end " >:: test_can_get_substring_from_end_when_zipper_is_at_end ;
-
+    "test_can_get_a_substring_from_start_when_zipper_is_at_middle " >:: test_can_get_a_substring_from_start_when_zipper_is_at_middle ;
+    "test_can_get_substring_of_middle_when_zipper_is_at_middle" >:: test_can_get_substring_of_middle_when_zipper_is_at_middle ;
+    "test_can_get_a_substring_over_a_split_piece" >:: test_can_get_a_substring_over_a_split_piece ;
+    "text_edit_1 " >:: text_edit_1 ;
+    "text_edit_2 " >:: text_edit_2 ;
 ]
