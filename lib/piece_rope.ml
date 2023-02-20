97ed5f1c716fc1e295d46955cedbcb301571ef59
Humza Shahid <humza@pop-os.localdomain>
2023-02-20 22:02:24 +0000

    make reference to buffer (not buffer itself) mutable so structure uses less memory. Latest buffer is compatible with all previous buffers so we can change the reference and let the old versions be GCed

--- lib/piece_rope.ml
+++ lib/piece_rope.ml
@@ -1,5 +1,5 @@
 type t = {
-  buffer: Piece_buffer.t;
+  mutable buffer: Piece_buffer.t;
   pieces: Piece_tree.t;
 }
 
@@ -26,23 +26,23 @@ let rec find_line_breaks (str: string) strLengthMinus1 pcStart pos acc =
 let insert index (str: string) piecerope =
   let pcStart = Piece_buffer.size piecerope.buffer in
   let pcLines = find_line_breaks str (String.length str - 1) pcStart 0 [] in
-  let buffer = Piece_buffer.append str piecerope.buffer in
+  let _ = piecerope.buffer <- Piece_buffer.append str piecerope.buffer in
   let pieces = Piece_tree.insert_tree index pcStart (String.length str) pcLines piecerope.pieces in
-  { buffer; pieces }
+  {piecerope with pieces = pieces}
 
 let prepend (str: string) piecerope =
   let pcStart = Piece_buffer.size piecerope.buffer in
   let pcLines = find_line_breaks str (String.length str - 1) pcStart 0 [] in
-  let buffer = Piece_buffer.append str piecerope.buffer in
+  let _ = piecerope.buffer <- Piece_buffer.append str piecerope.buffer in
   let pieces = Piece_tree.prepend pcStart (String.length str) pcLines piecerope.pieces in
-  { buffer; pieces }
+  {piecerope with pieces = pieces}
 
 let append (str: string) piecerope =
   let pcStart = Piece_buffer.size piecerope.buffer in
   let pcLines = find_line_breaks str (String.length str - 1) pcStart 0 [] in
-  let buffer = Piece_buffer.append str piecerope.buffer in
+  let _ = piecerope.buffer <- Piece_buffer.append str piecerope.buffer in
   let pieces = Piece_tree.append pcStart (String.length str) pcLines piecerope.pieces in
-  { buffer; pieces }
+  {piecerope with pieces = pieces}
 
 let delete start length piecerope =
   let pieces = Piece_tree.delete_tree start length piecerope.pieces in
