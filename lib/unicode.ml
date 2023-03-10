4b4947baf45f71a7e123d74897b1955bccdd923e
Humza Shahid <humza@pop-os.localdomain>
2023-03-10 11:29:56 +0000

    snake case consistency in unicode.ml

--- lib/unicode.ml
+++ lib/unicode.ml
@@ -1,4 +1,9 @@
-(* Functions for counting code points and line breaks. *)
+type encoding = Utf8 | Utf16 | Utf32
+
+type index_offsets = { utf8_pos: int; utf16_pos: int; utf32_pos: int; }
+
+let create_offsets utf8_pos utf16_pos utf32_pos = 
+  { utf8_pos; utf16_pos; utf32_pos; }
 
 (** Given the first byte of a UTF-8 code point, returns the length of that character in UTF-8. *) 
 let utf8_length chr =
@@ -9,7 +14,10 @@ let utf8_length chr =
   | '\xf0' .. '\xf7' -> 4
   | _ -> failwith "invalid utf-8 start"
 
-(** Given the first byte of a UTF-8 code point, returns the length of that character in UTF-16. *) 
+(** Given the first byte of a UTF-8 code point, returns the length of that character in UTF-16. 
+    This is almost identical to the utf8_length function above and that function can support a tuple,
+    but the additional allocation a tuple has bothered me.
+ *) 
 let utf16_length chr =
   match chr with
   | '\x00' .. '\xef' -> 1
@@ -18,78 +26,104 @@ let utf16_length chr =
 
 (** Counts the length of the string in UTF-16 and Unicode code points, 
     and builds an array of line breaks in terms of UTF-8 as that is OCaml's native string encoding. *)
-let count_string_stats (str: string) (pcStart: int) =
-  let rec get utf8Pos utf16Cntr utf32Cntr lineBreaks prevIsCr =
-    if utf8Pos >= String.length str then
-      utf16Cntr, utf32Cntr, lineBreaks |> List.rev |> Array.of_list
+let count_string_stats (str: string) (buffer_length: int) =
+  let rec get utf8_pos utf16_cntr utf32_cntr line_breaks prev_is_cr =
+    if utf8_pos >= String.length str then
+      utf16_cntr, utf32_cntr, (line_breaks |> List.rev |> Array.of_list)
     else
-      let chr = String.unsafe_get str utf8Pos in
-      let utf8Length = utf8_length chr in
-      let utf16Length = utf16_length chr in
+      let chr = String.unsafe_get str utf8_pos in
+      let utf8_length  = utf8_length  chr in
+      let utf16_length = utf16_length chr in
 
-      let lineBreaks =
-        if chr = '\r' || (chr = '\n' && not prevIsCr) then
-          (utf32Cntr + pcStart)::lineBreaks
+      let line_breaks =
+        if chr = '\r' || (chr = '\n' && not prev_is_cr) then
+          (utf32_cntr + buffer_length)::line_breaks
         else
-          lineBreaks
+          line_breaks
       in
-      get (utf8Pos + utf8Length) (utf16Cntr + utf16Length) (utf32Cntr + 1) lineBreaks (chr = '\n')
+      get (utf8_pos + utf8_length) (utf16_cntr + utf16_length) (utf32_cntr + 1) line_breaks (chr = '\n')
   in
   get 0 0 0 [] false
 
-(* Gets a substring from a string that has a non-ASCII character inside it.*)
 let utf32_sub (str: string) (start: int) (length: int) =
   let finish = start + length in
-  let rec sub strPos cdPos strStart strFinish =
-    if strPos = String.length str then
-      String.sub str strStart (String.length str - strStart)
+  let rec sub str_pos cd_pos str_start str_finish =
+    if str_pos = String.length str then
+      String.sub str str_start (String.length str - str_start)
     else
-      let strStart = 
-        if cdPos = start then
-          strPos
+      let str_start = 
+        if cd_pos = start then
+          str_pos
         else
-          strStart
+          str_start
       in
-      if cdPos = finish then
-        String.sub str strStart (strPos - strStart)
+      if cd_pos = finish then
+        String.sub str str_start (str_pos - str_start)
       else
-        let utf8Length = utf8_length (String.unsafe_get str strPos) in
-        sub (strPos + utf8Length) (cdPos + 1) strStart strFinish
+        let utf8_length = utf8_length (String.unsafe_get str str_pos) in
+        sub (str_pos + utf8_length) (cd_pos + 1) str_start str_finish
   in
   sub 0 0 0 0
 
-type index_offsets = { utf8_pos: int; utf16_pos: int; utf32_pos: int; }
+(* The count_to functions duplicate some code intentionally. 
+   A previous generic version existed, 
+   but I was bothered by additional if-statements on each recursion to handle UTF-8/16 cases when the specified index is inside a code point. *)
+let count_to_utf32 (str: string) (count_to: int) =
+  let rec cnt utf8_pos utf16_pos utf32_pos =
+    if utf32_pos = count_to then
+      create_offsets utf8_pos utf16_pos utf32_pos
+    else
+      let chr = String.unsafe_get str utf8_pos in
+      let u8_length  = utf8_length  chr in
+      let u16_length = utf16_length chr in
 
-let create_offsets utf8_pos utf16_pos utf32_pos = 
-  { utf8_pos; utf16_pos; utf32_pos; }
-
-type encoding = Utf8 | Utf16 | Utf32
-
-let count_to (str: string) (countTo: int) (enc: encoding) =
-  let rec cnt utf8Pos utf16Pos utf32Pos =
-    match enc with
-    | Utf8 when utf8Pos = countTo ->
-      { utf8_pos = utf8Pos; utf16_pos = utf16Pos; utf32_pos = utf32Pos; }
-    | Utf16 when utf16Pos = countTo ->
-      { utf8_pos = utf8Pos; utf16_pos = utf16Pos; utf32_pos = utf32Pos; }
-    | Utf32 when utf32Pos = countTo ->
-      { utf8_pos = utf8Pos; utf16_pos = utf16Pos; utf32_pos = utf32Pos; }
-    | _ ->
-      let chr = String.unsafe_get str utf8Pos in
-      let utf8Length = utf8_length chr in
-      let utf16Length = utf16_length chr in
-
-      let nextUtf8 = utf8Pos + utf8Length in
-      let nextUtf16 = utf16Pos + utf16Length in
-      let nextUtf32 = utf32Pos + 1 in
-      (* Match to see if next pos is over and clip to current if so. Else, recurse. *)
-      (match enc with
-      | Utf8 when nextUtf8 > countTo ->
-        { utf8_pos = utf8Pos; utf16_pos = utf16Pos; utf32_pos = utf32Pos; }
-      | Utf16 when nextUtf16 > countTo ->
-        { utf8_pos = utf8Pos; utf16_pos = utf16Pos; utf32_pos = utf32Pos; }
-      | _ ->
-        cnt nextUtf8 nextUtf16 nextUtf32)
+      let nextUtf8  = utf8_pos  + u8_length in
+      let nextUtf16 = utf16_pos + u16_length in
+      let nextUtf32 = utf32_pos + 1 in
+      cnt nextUtf8 nextUtf16 nextUtf32
   in
   cnt 0 0 0
 
+let count_to_utf16 (str: string) (count_to: int) =
+  let rec cnt utf8_pos utf16_pos utf32_pos =
+    if utf16_pos = count_to then
+      create_offsets utf8_pos utf16_pos utf32_pos
+    else
+      let chr = String.unsafe_get str utf8_pos in
+      let u8_length = utf8_length chr in
+      let u16_length = utf16_length chr in
+
+      let next_u8  = utf8_pos  + u8_length in
+      let next_u16 = utf16_pos + u16_length in
+      let next_u32 = utf32_pos + 1 in
+      if next_u16 > count_to then
+        create_offsets utf8_pos utf16_pos utf32_pos
+      else
+        cnt next_u8 next_u16 next_u32
+  in
+  cnt 0 0 0
+
+let count_to_utf8 (str: string) (count_to: int) =
+  let rec cnt utf8_pos utf16_pos utf32_pos =
+    if utf8_pos = count_to then
+      create_offsets utf8_pos utf16_pos utf32_pos
+    else
+      let chr = String.unsafe_get str utf8_pos in
+      let u8_length = utf8_length chr in
+      let u16_length = utf16_length chr in
+
+      let next_u8 = utf8_pos + u8_length in
+      let next_u16 = utf16_pos + u16_length in
+      let next_u32 = utf32_pos + 1 in
+      if next_u8 > count_to then
+        create_offsets utf8_pos utf16_pos utf32_pos
+      else
+        cnt next_u8 next_u16 next_u32
+  in
+  cnt 0 0 0
+
+let count_to (str: string) (count_towards: int) (enc: encoding) =
+  match enc with
+  | Utf8  ->  count_to_utf8  str count_towards
+  | Utf16 ->  count_to_utf16 str count_towards
+  | Utf32 ->  count_to_utf32 str count_towards
