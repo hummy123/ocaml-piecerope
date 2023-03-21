0e9f4ff151d82ed1134742036ad9b98ae9b4a2d1
Humza Shahid <humza@pop-os.localdomain>
2023-03-21 20:11:34 +0000

    refactor benchmark file

--- bench/bench.ml
+++ bench/bench.ml
@@ -1,19 +1,28 @@
-let () =
-  (* 
-     Function to insert into a structure, returning the structure modified.
-     First parameter: index to insert at.
-     Second: string to insert.
-     Third: the structure (it acts as an accumulator value).
-   *)
-  let piece_rope_insert pos ins_str rope = Piece_rope.insert pos ins_str rope in
-  (* 
-     Function to delete from a structure, returning the structure modified.
-     First parameter: index to start deleting from.
-     Second: length to delete.
-     Third: the structure (it acts as an accumulator value).
-   *)
-  let piece_rope_delete pos del_num rope = Piece_rope.delete pos del_num rope in
+(* 
+   Function to insert into a structure, returning the structure modified.
+   First parameter: index to insert at.
+   Second: string to insert.
+   Third: the structure (it acts as an accumulator value).
+ *)
+let piece_rope_insert pos ins_str rope = Piece_rope.insert pos ins_str rope in
+(* 
+   Function to delete from a structure, returning the structure modified.
+   First parameter: index to start deleting from.
+   Second: length to delete.
+   Third: the structure (it acts as an accumulator value).
+ *)
+let piece_rope_delete pos del_num rope = Piece_rope.delete pos del_num rope in
 
+(* Zed_rope insertion and deletion functions. *)
+let zed_insert pos (ins_str: string) rope =
+  ((ins_str |> Zed_string.of_utf8 |> Zed_rope.of_string) |> Zed_rope.insert rope pos)
+in
+
+let zed_delete pos del_num rope =
+  Zed_rope.remove rope pos del_num
+in
+
+let () =
   (* Below functions run the edit traces with each dataset. *)
   let _ =
     Utils.run_txns_time 
@@ -37,14 +46,6 @@ let () =
   in
 
   (* Running the datasets on Zed. *)
-  let zed_insert pos (ins_str: string) rope = 
-    let ins_str = Zed_string.of_utf8 ins_str in
-    Zed_rope.insert rope pos (Zed_rope.of_string ins_str)
-  in
-  let zed_delete pos del_num rope =
-    Zed_rope.remove rope pos del_num
-  in
-
   let _ =
     Utils.run_txns_time "Svelte Zed" Sveltecomponent.data (Zed_rope.empty())
       zed_insert zed_delete
@@ -61,4 +62,6 @@ let () =
     Utils.run_txns_time "Automerge Zed" Automerge.data (Zed_rope.empty())
       zed_insert zed_delete
   in
-  ()
+  ();
+in ()
+
