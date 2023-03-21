47918b941e1b8a6d157f20e2b4100d780a496663
Humza Shahid <humza@pop-os.localdomain>
2023-03-21 19:47:35 +0000

    add zed benchmarks

--- bench/bench.ml
+++ bench/bench.ml
@@ -35,4 +35,30 @@ let () =
     Utils.run_txns_time "Automerge Piece_rope" Automerge.data Piece_rope.empty
       piece_rope_insert piece_rope_delete
   in
+
+  (* Running the datasets on Zed. *)
+  let zed_insert pos (ins_str: string) rope = 
+    let ins_str = Zed_string.of_utf8 ins_str in
+    Zed_rope.insert rope pos (Zed_rope.of_string ins_str)
+  in
+  let zed_delete pos del_num rope =
+    Zed_rope.remove rope pos del_num
+  in
+
+  let _ =
+    Utils.run_txns_time "Svelte Zed" Rustcode.data (Zed_rope.empty())
+      zed_insert zed_delete
+  in
+  let _ =
+    Utils.run_txns_time "Rustcode Zed" Rustcode.data (Zed_rope.empty())
+      zed_insert zed_delete
+  in
+  let _ =
+    Utils.run_txns_time "Sephblog Zed" Sephblog.data (Zed_rope.empty())
+      zed_insert zed_delete
+  in
+  let _ =
+    Utils.run_txns_time "Automerge Zed" Automerge.data (Zed_rope.empty())
+      zed_insert zed_delete
+  in
   ()
